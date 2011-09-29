4f11b8eab160a3baa799de7ca9d2d3078ce891a2
Deborah Johnson <deborah.max.johnson@gmail.com>
2011-09-29 15:34:01 -0400

    Reformatted comments / section divisions. Modified output file section

--- dsi_studio_track.m
+++ dsi_studio_track.m
@@ -1,37 +1,51 @@
-function dsi_studio_track(fibfile,seedfile,roi,roi2,output);
+% function dsi_studio_track(fibfile,seedfile,roi,roi2,output); 
 
-% Set path for DSI Studio
-[dsi_studio, dsi_studio_path] = uigetfile('C:\Users\*.exe','Select DSI Studio');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   Set Paths for Files   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[dsi_studio, dsi_studio_path] = uigetfile('C:\Users\*.exe','Select DSI Studio'); % Path for DSI Studio
 dsi_studio_pointer = sprintf('%s%s',dsi_stuio,dsi_stuido_path);
 
-% Set path for the .fib file
-[fib, fibpath] = uigetfile('C:\Users\*.fib.gz','Select the .fib file');
+[fib, fibpath] = uigetfile('C:\Users\*.fib.gz','Select the .fib file'); % Path for .fib file
 fibfile = sprintf('%s%s',fib,fibpath);
 
-% Set path for the seed file
-[seed, seedpath] = uigetfile('C:\Users\*.nii','Select the seed file');
+[seed, seedpath] = uigetfile('C:\Users\*.nii','Select the seed file'); % Path for the seed file
 seedfile = sprintf('%s%s',fib,fibpath);
 
+%%======================================================     Paths for ROI Files
+
 % Set paths for first and second ROI files
-[roifile, roipath] = uigetfile('C:\Users\*.nii','Select first ROI file');
+[roifile, roipath] = uigetfile('C:\Users\*.nii','Select first ROI file'); 
 roi = sprintf('%s%s',roipath,roifile);
 [roi2file, roi2path] = uigetfile('C:\Users\*.nii','Select second ROI file');
 roi2 = sprintf('%s%s',roi2path,roi2file);
 
-% Create array to store paths for first and second ROI files
-roi_pairs = {roi, roi2};
+roi_pairs = {roi, roi2}; % Create array to store paths for first two ROI files
 
-% Loop for adding pairs of ROI files
+% Loop for adding more pairs of ROI files
 while isequal(questdlg('Would you like to select more ROI pairs?','Select more?'), 'Yes');
 	
 	[roifile, roipath] = uigetfile('C:\Users\*.nii','Select first ROI file');
 	roi = sprintf('%s%s',roipath,roifile);
 	[roi2file, roi2path] = uigetfile('C:\Users\*.nii','Select first ROI file');
 	roi2 = sprintf('%s%s',roi2path,roi2file);
-	
-	% Concatenate pairs of ROI pahts into roi_pairs cell array
-	roi_pairs = cat(1, roi_pairs, {roi, roi2}); 
+
+	roi_pairs = cat(1, roi_pairs, {roi, roi2}); % Concatenate pairs of ROI pahts into roi_pairs cell array
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   Output File   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Specifies location for output file
+output_dir = uigetdir('C:\Users\*.*','Select location for output file');
+extension = questdlg('Select a format for output file','Output File Format','.trk','.txt')
+output = sprintf('%soutput%s',output_dir,extension)
+
+if isequal(format_quest, '.trk')
+	
+else
+	
+end
+
+
 
 % Loop for executing DSI Studio command in command prompt
 for i = 1:size(roi_pairs, 1)
@@ -39,6 +53,3 @@ for i = 1:size(roi_pairs, 1)
 end
 
 eval(strn);
-
-% Specifies output file format and path
-output = sprintf('%strack.trk',fibpath)
