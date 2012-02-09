9063a35439319797f6850f061346056baf10c798
Deborah Johnson <deborah.max.johnson@gmail.com>
2012-02-09 13:14:59 -0500

    Fixed open_GUI issue

--- Full GUI/BatchFiberTrackingGUI.m
+++ Full GUI/BatchFiberTrackingGUI.m
@@ -1,26 +1,26 @@
-function varargout = open_GUI(varargin)
-% OPEN_GUI M-file for open_GUI.fig
-%      OPEN_GUI, by itself, creates a new OPEN_GUI or raises the existing
+function varargout = BatchFiberTrackingGUI(varargin)
+% BatchFiberTrackingGUI M-file for BatchFiberTrackingGUI.fig
+%      BatchFiberTrackingGUI, by itself, creates a new BatchFiberTrackingGUI or raises the existing
 %      singleton*.
 %
-%      H = OPEN_GUI returns the handle to a new OPEN_GUI or the handle to
+%      H = BatchFiberTrackingGUI returns the handle to a new BatchFiberTrackingGUI or the handle to
 %      the existing singleton*.
 %
-%      OPEN_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in OPEN_GUI.M with the given input arguments.
+%      BatchFiberTrackingGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in BatchFiberTrackingGUI.M with the given input arguments.
 %
-%      OPEN_GUI('Property','Value',...) creates a new OPEN_GUI or raises the
+%      BatchFiberTrackingGUI('Property','Value',...) creates a new BatchFiberTrackingGUI or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before open_GUI_OpeningFcn gets called.  An
+%      applied to the GUI before BatchFiberTrackingGUI_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to open_GUI_OpeningFcn via varargin.
+%      stop.  All inputs are passed to BatchFiberTrackingGUI_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help open_GUI
+% Edit the above text to modify the response to help BatchFiberTrackingGUI
 
 % Last Modified by GUIDE v2.5 16-Nov-2011 12:50:10
 
@@ -28,8 +28,8 @@ function varargout = open_GUI(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @open_GUI_OpeningFcn, ...
-                   'gui_OutputFcn',  @open_GUI_OutputFcn, ...
+                   'gui_OpeningFcn', @BatchFiberTrackingGUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @BatchFiberTrackingGUI_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before open_GUI is made visible.
-function open_GUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before BatchFiberTrackingGUI is made visible.
+function BatchFiberTrackingGUI_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to open_GUI (see VARARGIN)
+% varargin   command line arguments to BatchFiberTrackingGUI (see VARARGIN)
 
-% Choose default command line output for open_GUI
+% Choose default command line output for BatchFiberTrackingGUI
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes open_GUI wait for user response (see UIRESUME)
+% UIWAIT makes BatchFiberTrackingGUI wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = open_GUI_OutputFcn(hObject, eventdata, handles) 
+function varargout = BatchFiberTrackingGUI_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -73,9 +73,9 @@ function varargout = open_GUI_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in open_GUI.
-function open_GUI_Callback(hObject, eventdata, handles)
-% hObject    handle to open_GUI (see GCBO)
+% --- Executes on button press in BatchFiberTrackingGUI.
+function BatchFiberTrackingGUI_Callback(hObject, eventdata, handles)
+% hObject    handle to BatchFiberTrackingGUI (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
